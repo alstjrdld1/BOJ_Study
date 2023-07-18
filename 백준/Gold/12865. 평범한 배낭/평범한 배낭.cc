599cb4973a4380a8d76e35947e84c5afea0a12f8
minstone_98 <50548788+alstjrdld1@users.noreply.github.com>
2023-07-18 21:53:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 20 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <cstring>
 #include <vector>
 using namespace std;
 
@@ -8,13 +7,9 @@ int main() {
 
 	int N, K;
 	cin >> N >> K;
-	int** dp = new int* [N+1];
-	for (int i = 0; i < N+1; i++) {
-		dp[i] = new int[K+1];
-		for (int j = 0; j < K + 1; j++) {
-			dp[i][j] = 0;
-		}
-	}
+
+	vector<int> dp(K + 1);
+	vector<int> dpPrev(K + 1);
 
 	vector<int> w(N+1);
 	vector<int> v(N+1);
@@ -24,17 +19,15 @@ int main() {
 	}
 
 	for (int i = 1; i <= N; i++) {
+		dpPrev = dp;
 		for (int j = 1; j <= K; j++) {
 			if ((j - w[i]) >= 0) {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-			}
-			else {
-				dp[i][j] = dp[i - 1][j];
+				dp[j] = max(dpPrev[j], dpPrev[j - w[i]] + v[i]);
 			}
 		}
 	}
 
-	cout << dp[N][K];
+	cout << dp[K];
 
 	return 0;
 }
