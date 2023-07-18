b24b383b87f7bb6b11853d1263e069766d968f03
minstone_98 <50548788+alstjrdld1@users.noreply.github.com>
2023-07-18 21:58:25 +0900

    [Gold V] Title: 평범한 배낭, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -11,15 +11,13 @@ int main() {
 	vector<int> dp(K + 1);
 	vector<int> dpPrev(K + 1);
 
-	vector<int> w(N+1);
-	vector<int> v(N+1);
-
 	for (int i = 1; i <= N; i++) {
-		cin >> w[i] >> v[i];
+		int w, v;
+		cin >> w >> v;
 		dpPrev = dp;
 		for (int j = 1; j <= K; j++) {
-			if ((j - w[i]) >= 0) {
-				dp[j] = max(dpPrev[j], dpPrev[j - w[i]] + v[i]);
+			if ((j - w) >= 0) {
+				dp[j] = max(dpPrev[j], dpPrev[j - w] + v);
 			}
 		}
 	}
