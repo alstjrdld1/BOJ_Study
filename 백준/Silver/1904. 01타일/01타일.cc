d64ab947271c2faad283ec0e7d181c18ea10b526
minstone_98 <50548788+alstjrdld1@users.noreply.github.com>
2023-07-19 20:17:27 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -2,20 +2,31 @@
 using namespace std;
 
 #define MOD 15746
-int dp[1000001];
 
 int main() {
 
 	int n;
 	cin >> n;
+	int dp[2];
+	dp[0] = 1;
+	dp[1] = 2;
 
-	dp[1] = 1;
-	dp[2] = 2;
-	for (int i = 3; i <= n; i++) {
-		dp[i] = (dp[i - 1] % MOD + dp[i - 2] % MOD) % MOD;
+	int result = 0;
+	if (n == 1) {
+		result = 1;
+	}
+	else if (n == 2) {
+		result = 2;
+	}
+	else {
+		for (int i = 3; i <= n; i++) {
+			result = (dp[1] % MOD + dp[0] % MOD) % MOD;
+			dp[0] = dp[1];
+			dp[1] = result % MOD;
+		}
 	}
 
-	cout << dp[n] % MOD;
+	cout << result % MOD;
 
 	return 0;
 }
