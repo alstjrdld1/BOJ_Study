9118fdf9d61f9b16ca78b578fb5a93832dd00578
minstone_98 <50548788+alstjrdld1@users.noreply.github.com>
2024-10-07 22:13:39 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <cmath>
 using namespace std;
 
 int main()
@@ -10,23 +8,20 @@ int main()
 	cin >> H >> Y;
 
 	double money[11] = { 0 };
-	double interest[3] = { 1.05, 1.2, 1.35 };
 	money[0] = H;
 
 	for (int i = 1; i <= 10; i++)
 	{
-		vector<double> interests;
-		interests.push_back(floor(money[i - 1] * interest[0]));
+		money[i] = int(money[i - 1] * 1.05);
 
 		if (i >= 3)
 		{
-			interests.push_back(floor(money[i - 3] * interest[1]));
+			money[i] = int(max(money[i], money[i - 3] * 1.2));
 		}
 		if (i >= 5)
 		{
-			interests.push_back(floor(money[i - 5] * interest[2]));
+			money[i] = int(max(money[i], money[i - 5] * 1.35));
 		}
-		money[i] = *max_element(interests.begin(), interests.end());
 	}
 	
 	cout << money[Y];
